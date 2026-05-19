d8a1fb9e91f0727d41f2391f3a663adb486df728
莯凛 <mokurin000@gmail.com>
2026-05-20 00:42:15 +0800

    chore: blank hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,20 @@
-import 'dart:convert';
-import 'dart:io';
+import 'package:flutter/material.dart';
 
-import 'package:pvzmdz_editor/src/game_data.dart';
+void main() {
+  runApp(const MainApp());
+}
 
-Future<void> main() async {
-  const inputPath =
-      r'C:\Users\moku\AppData\LocalLow\MiaoDouzi\抽卡版PVZ\save.json';
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  const outputPath =
-      r'C:\Users\moku\AppData\LocalLow\MiaoDouzi\抽卡版PVZ\save1.json';
-
-  try {
-    // 读取 JSON 文件
-    final jsonString = await File(inputPath).readAsString();
-
-    // 解析 JSON
-    final jsonMap = jsonDecode(jsonString) as Map<String, dynamic>;
-
-    // 反序列化
-    final gameData = GameData.fromJson(jsonMap);
-
-    // 再序列化
-    final encoded = const JsonEncoder.withIndent(
-      '  ',
-    ).convert(gameData.toJson());
-
-    // 写入新文件
-    await File(outputPath).writeAsString(encoded);
-
-    print('转换完成');
-    print('输入: $inputPath');
-    print('输出: $outputPath');
-  } catch (e, stackTrace) {
-    print('处理失败: $e');
-    print(stackTrace);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('抽卡版')),
+        body: Center(child: Text('Hello world')),
+        backgroundColor: Colors.blueGrey.shade100,
+      ),
+    );
   }
 }
